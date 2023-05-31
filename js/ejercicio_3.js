b3c24b60e4c942c125178615eb05054033ab3f95
angie0611 <128073076+angie0611@users.noreply.github.com>
2023-05-31 17:22:07 -0300

    Add files via upload

---
+++ js/ejercicio_3.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/* Tarea
+ * Objetivos: adquirir herramientas para identificar 
+ * problemas en el código, identificando si se producen
+ * errores de sintaxis o de lógica.
+ * debe descomentar cada ejercicio!
+ */
+ 
+ //-------------------------------------------------------------------
+// 3. El siguiente codigo tiene un error, de qué se trata?
+//    proponga una alternativa para que se imprima 1 en consola, 
+//    manteniendo las operaciones de división.
+
+
+let number=  25  ;
+let denom=  25  ;
+let resultado= number / denom ;
+
+console.log( resultado )
---
+++ js/ejercicio_4.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/* Tarea
+ * Objetivos: adquirir herramientas para identificar 
+ * problemas en el código, identificando si se producen
+ * errores de sintaxis o de lógica.
+ * debe descomentar cada ejercicio!
+ */
+ 
+ //-------------------------------------------------------------------
+// 4. Realice un acumulador utilizando una variable, debe acumular en 5 lineas
+//    de código el numero 100, comenzando desde el 1.
+
+
+let acumulador = 1; //No utilizar mas variables ;D
+//primer paso: 
+acumulador += 19
+
+//segundo paso:
+acumulador += 20
+
+//tercer paso:
+acumulador += 20
+
+//cuarto paso:
+acumulador +=20
+
+//quinto paso:
+acumulador += 20
+
+//Imprimimos en consola:
+console.log("El resultado del programa es: " + acumulador); 
+// El resultado debe ser 100
+
